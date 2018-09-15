cb2c47fe336b88d9568566e1ef37a8903348788f
Andreas Stöckel <astoecke@uwaterloo.ca>
2018-09-15 14:49:13 -0400

    Fix C++ support, cleanup code

--- foxen/stack.c
+++ foxen/stack.c
@@ -19,10 +19,8 @@
 #include <assert.h>
 #include <stdint.h>
 
+#ifndef FX_NO_CONFIG
 #include "config.h"
-
-#ifdef FX_WITH_VALGRIND
-#include <valgrind.h>
 #endif
 
 #include <foxen/stack.h>
@@ -63,22 +61,22 @@ typedef struct {
 	std::exception_ptr eptr;
 } fx_stack_exception_stub_data_t;
 
-static __attribute__((noinline)) void *_fx_stack_exception_stub(void *data_)
-{
+static __attribute__((noinline)) void *_fx_stack_exception_stub(void *data_) {
 	/* Type-convert the callback data. */
-	fx_stack_stub_data_t *data = (fx_stack_stub_data_t *)data_;
+	fx_stack_exception_stub_data_t *data =
+	    (fx_stack_exception_stub_data_t *)data_;
 
 	/* Call the callback within an explicit try-catch block. This will make sure
 	   that exceptions are caught, even on weird architectures such as ARM which
 	   do special exception handling voodoo. */
 	try {
 		return data->cback(data->data);
-	}
-	catch (...) {
+	} catch (...) {
 		/* There was an exception! Move the exception into the exception_ptr
 		   instance. The calling code rethrows the exception. */
 		data->eptr = std::current_exception();
 	}
+	return nullptr;
 }
 
 #endif /* FX_WITH_CPP_EXCEPTIONS */
@@ -87,39 +85,54 @@ static __attribute__((noinline)) void *_fx_stack_exception_stub(void *data_)
  * Common stack switching code                                               *
  *****************************************************************************/
 
+/*
+ * Define valgrind-related macros
+ */
+#ifdef FX_WITH_VALGRIND
+
+#include <valgrind.h>
+
+#define FX_VALGRIND_STACK_REGISTER                                    \
+	/* Inform valgrind that the given memory region is a new stack */ \
+	const int stack_id = VALGRIND_STACK_REGISTER(stack_start, stack_end);
+
+#define FX_VALGRIND_STACK_UNREGISTER                                  \
+	/* Inform valgrind that the given memory region is a new stack */ \
+	VALGRIND_STACK_DEREGISTER(stack_id);
+
+#else /* FX_WITH_VALGRIND */
+
+#define FX_VALGRIND_STACK_REGISTER
+#define FX_VALGRIND_STACK_UNREGISTER
+
+#endif /* FX_WITH_VALGRIND */
+
 void *fx_stack_switch(void *stack_start, void *stack_end, void *stack_ptr,
-                      fx_stack_cback cback, void *data)
-{
+                      fx_stack_cback cback, void *data) {
 	/* Make sure the given stack pointer is sane */
 	assert((stack_start < stack_ptr) && (stack_ptr <= stack_end));
 
-#ifdef FX_WITH_VALGRIND
-	/* Inform valgrind that the given memory region is a new stack */
-	const int stack_id = VALGRIND_STACK_REGISTER(stack_start, stack_end);
-#endif /* FX_WITH_VALGRIND */
-
 #ifdef FX_WITH_CPP_EXCEPTIONS
-	{
-		/* Call the platform-specific assembly function wrapped in the given
-		   stub function */
-		fx_stack_exception_stub_data_t stub_data{cback, data, nullptr};
-		void *result = _fx_stack_switch(stack_ptr, _fx_stack_stub, &stub_data);
+	/* Call the platform-specific assembly function wrapped in the given
+	   stub function */
+	fx_stack_exception_stub_data_t stub_data{cback, data, nullptr};
 
-		/* Re-throw any C++ exception that was thrown by the code inside the
-		   alternative stack. */
-		if (stub_data.eptr) {
-			std::rethrow_exception(stub_data.eptr);
-		}
+	FX_VALGRIND_STACK_REGISTER
+	void *result =
+	    _fx_stack_switch(stack_ptr, _fx_stack_exception_stub, &stub_data);
+	FX_VALGRIND_STACK_UNREGISTER
+
+	/* Re-throw any C++ exception that was thrown by the code inside the
+	   alternative stack. */
+	if (stub_data.eptr) {
+		std::rethrow_exception(stub_data.eptr);
 	}
-#else
+#else  /* FX_WITH_CPP_EXCEPTIONS */
 	/* Call the platform-specific assembly function */
+	FX_VALGRIND_STACK_REGISTER
 	void *result = _fx_stack_switch(stack_ptr, cback, data);
+	FX_VALGRIND_STACK_UNREGISTER
 #endif /* FX_WITH_CPP_EXCEPTIONS */
-
-#ifdef FX_WITH_VALGRIND
-	/* Inform valgrind that this memory region is no longer used as a stack */
-	VALGRIND_STACK_DEREGISTER(stack_id);
-#endif /* FX_WITH_VALGRIND */
 
 	return result;
 }
